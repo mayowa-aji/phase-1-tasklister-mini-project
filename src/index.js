2144001d8ca77c9d2388a4c321d3e6ec07d8f385
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-01-12 16:26:55 -0500

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.querySelector('#create-task-form');
+  taskForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const descField = document.querySelector('#new-task-description');
+    // console.log(descField.value);
+
+
+    const taskList = document.querySelector('#tasks');
+    // console.log(taskList)
+    const newTask = document.createElement('li');
+    newTask.textContent = descField.value;
+
+    taskList.append(newTask);
+  })
 });
